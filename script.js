34134dfcb8453ca4679a77a5b1ea85205834e9a5
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-11 16:04:20 +0100

    costo del biglietto in pagina

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-// INPUT
-// quanti km vuole percorrere il passeggero
-let numKm = document.getElementById("numKm").value
-console.log("Km da percorrere:", numKm);
+// OUTPUT
+const myForm = document.getElementById("myForm");
 
-// età del passeggero
-let sceltaEta = document.getElementById("sceltaEta").value
-console.log("età del passeggero:", etaPasseggero);
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // INPUT
+    // quanti km vuole percorrere il passeggero
+    let numKm = document.getElementById("numKm").value
+    console.log("Km da percorrere:", numKm);
+
+    // età del passeggero
+    let sceltaEta = document.getElementById("sceltaEta").value
+    console.log("età del passeggero:", sceltaEta);
+
+    // prezzo totale del biglietto per il viaggio
+    let bigliettoBase = numKm * 0.21;
+
+    // sconto del biglietto in base all'età del passeggero
+    let bigliettoFinale;
+
+    if (sceltaEta === "minorenne") {
+        let bigliettoFinale = bigliettoBase * 0.8; // sconto del 20%
+    } else if (sceltaEta === "over-65") {
+        bigliettoFinale = bigliettoBase * 0.6; // sconto del 40%
+    } else {
+        bigliettoFinale = bigliettoBase;
+    }
+
+    console.log("Costo del biglietto", bigliettoFinale)
+
+    // // OUTPUT in pagina
+    document.getElementById("costoBiglietto").innerHTML = `<h4>Costo biglietto</h4> ${bigliettoFinale.toFixed(2)}`
+
+})
 
 
-// prezzo totale del biglietto per il viaggio
-const bigliettoBase = numKm * 0.21;
-console.log(bigliettoBase);
-
-// sconto del biglietto in base all'età del passeggero
-if (etaPasseggero < 18) {
-    let bigliettoFinale = bigliettoBase * 0.8;
-    console.log("prezzo del biglietto", bigliettoFinale)
-} else if (etaPasseggero >= 65) {
-    bigliettoFinale = bigliettoBase * 0.6;
-    console.log("prezzo del biglietto", bigliettoFinale);
-} else {
-    bigliettoFinale = bigliettoBase;
-    console.log("prezzo del biglietto", bigliettoFinale);
-}
-
-// // OUTPUT in pagina
-// document.getElementById("prezzo-biglietto").textContent = `Prezzo del biglietto: €${bigliettoFinale.toFixed(2)}`
